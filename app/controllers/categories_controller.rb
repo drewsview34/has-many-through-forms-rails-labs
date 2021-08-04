9b40bee475f9987e7663ad8008b8d9e129db7dde
drewsview34 <drewsview34@outlook.com>
2021-08-04 11:22:38 -0700

    category shows all of the related posts

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,10 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def update
+    @category = Category.find(params[:id])
+    
+  end
+
 end
